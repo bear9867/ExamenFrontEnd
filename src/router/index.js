b041af7c39bca2edfb2938ee1bcd6020b214fe21
Benja <alfaro.benjaminr@gmail.com>
2026-07-14 22:56:28 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import CalificacionesView from '../views/CalificacionesView.vue'
 import RegistroView from '../views/RegistroView.vue'
 
 const router = createRouter({
-  history: createWebHistory('/ExamenFrontEnd/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -11,8 +11,8 @@ const router = createRouter({
       component: CalificacionesView,
     },
     {
-      path: '/Formulario de registro',
-      name: 'Formulario de registro',
+      path: '/formulario-registro',
+      name: 'formulario-registro',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
